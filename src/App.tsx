1c8955ade4ad9957928c649c5172599bdc35fc28
zxy <2544682494@qqcom>
2022-02-05 17:57:18 +0800

    Add image to chat ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,114 +4,132 @@ import Video from "./Video";
 import AgoraRTC, {IAgoraRTCRemoteUser, ICameraVideoTrack} from "agora-rtc-sdk-ng";
 import {APP_ID} from "./Settings";
 import AgoraRTM from "agora-rtm-sdk"
+
+interface Message {
+    memberId: string
+    text?: string
+    image?: string
+}
+
 function App() {
-    const agoraRTC = useMemo(()=>{
+    const agoraRTC = useMemo(() => {
         return AgoraRTC.createClient({mode: "rtc", codec: "vp8"});
-    },[])
+    }, [])
     const [channel, setChannel] = useState("")
-    const [users,setUsers]=useState<IAgoraRTCRemoteUser[]>([])
-    const [currentChannel,setCurrentChannel]=useState<string>()
-    const [account,setAccount]=useState("")
-    const [primaryVideo,setPrimaryVideo]=useState<ICameraVideoTrack>()
-    let agoraRTM = useMemo(()=>{
-       return  AgoraRTM.createInstance(APP_ID);
-    },[])
-    const [rtmChannel,setRtmChannel]=useState<any>()
-    const [logs,setLogs]=useState<string[]>([])
-    const [message,setMessage]=useState("")
-    let messageContainerRef = useRef<HTMLDivElement|null>();
-    useEffect(()=>{
-        let handler = (message:any, memberId:string)=>{
-            if (message.messageType === "TEXT"){
-                pushTextMessage(memberId,message.text)
+    const [users, setUsers] = useState<IAgoraRTCRemoteUser[]>([])
+    const [currentChannel, setCurrentChannel] = useState<string>()
+    const [account, setAccount] = useState("")
+    const [primaryVideo, setPrimaryVideo] = useState<ICameraVideoTrack>()
+    let agoraRTM = useMemo(() => {
+        return AgoraRTM.createInstance(APP_ID);
+    }, [])
+    const [rtmChannel, setRtmChannel] = useState<any>()
+    const [messages, setMessages] = useState<Message[]>([])
+    const [message, setMessage] = useState("")
+    let messageContainerRef = useRef<HTMLDivElement | null>();
+    useEffect(() => {
+        let handler = (message: any, memberId: string) => {
+            if (message.messageType === "TEXT") {
+                pushMessage({memberId, text: message.text})
+            } else if (message.messageType === "IMAGE") {
+                let fileReader = new FileReader();
+                fileReader.readAsDataURL(message.thumbnail);
+                fileReader.onload = ev => {
+                    pushMessage({memberId, image: fileReader.result as string})
+                }
             }
         };
-        rtmChannel?.on("ChannelMessage",handler)
-        return ()=>{
-            rtmChannel?.off("ChannelMessage",handler)
+        rtmChannel?.on("ChannelMessage", handler)
+        return () => {
+            rtmChannel?.off("ChannelMessage", handler)
         }
-    },[rtmChannel,logs])
-    function pushTextMessage(memberId:string,message:string){
-        const array = Array.from(logs)
-        array.push(`${memberId}: ${message}`)
-        setLogs(array)
-        messageContainerRef.current?.scrollTo({top:messageContainerRef.current!!.scrollHeight})
+    }, [rtmChannel, messages])
+
+    function pushMessage(message: Message) {
+        const array = Array.from(messages)
+        array.push(message)
+        setMessages(array)
+        messageContainerRef.current?.scrollTo({top: messageContainerRef.current!!.scrollHeight})
     }
-    function joinChannel(){agoraRTC.join(APP_ID, channel, null,).then(async () => {
-        setCurrentChannel(channel)
-        const [microphoneTrack, cameraTrack] = await AgoraRTC.createMicrophoneAndCameraTracks();
-        setPrimaryVideo(cameraTrack)
-        await agoraRTC.publish([cameraTrack,microphoneTrack])
-        await agoraRTM.login({uid:account})
-        let rtmChannel = agoraRTM.createChannel(channel);
-        setRtmChannel(rtmChannel)
-        await rtmChannel.join()
-        agoraRTC.remoteUsers.forEach(u=>{
-            if (u.hasVideo){
-                agoraRTC.subscribe(u,"video").then(track=>{
-                    const array = Array.from(users)
-                    array.push(u)
-                    setUsers(array)
-                })
-            }
-            if (u.hasAudio){
-                agoraRTC.subscribe(u,"audio").then(track=>{
-                    track.play()
-                })
-            }
-        })
 
-        agoraRTC.on("user-published", async (user, mediaType) => {
-            const lowStream = true
-            try {
-                await agoraRTC.setRemoteVideoStreamType(user.uid, lowStream ? 1 : 0)
-            } catch (e) {
-                console.error(e)
-            }
-
-            agoraRTC.subscribe(user, mediaType).then(() => {
-                if (mediaType === "video" && user.videoTrack) {
-                    const array = Array.from(users)
-                    array.push(user)
-                    setUsers(array)
+    function joinChannel() {
+        agoraRTC.join(APP_ID, channel, null,).then(async () => {
+            setCurrentChannel(channel)
+            const [microphoneTrack, cameraTrack] = await AgoraRTC.createMicrophoneAndCameraTracks();
+            setPrimaryVideo(cameraTrack)
+            await agoraRTC.publish([cameraTrack, microphoneTrack])
+            await agoraRTM.login({uid: account})
+            let rtmChannel = agoraRTM.createChannel(channel);
+            setRtmChannel(rtmChannel)
+            await rtmChannel.join()
+            agoraRTC.remoteUsers.forEach(u => {
+                if (u.hasVideo) {
+                    agoraRTC.subscribe(u, "video").then(track => {
+                        const array = Array.from(users)
+                        array.push(u)
+                        setUsers(array)
+                    })
                 }
-                if (mediaType === "audio") {
-                    user.audioTrack?.play()
+                if (u.hasAudio) {
+                    agoraRTC.subscribe(u, "audio").then(track => {
+                        track.play()
+                    })
                 }
             })
+
+            agoraRTC.on("user-published", async (user, mediaType) => {
+                const lowStream = true
+                try {
+                    await agoraRTC.setRemoteVideoStreamType(user.uid, lowStream ? 1 : 0)
+                } catch (e) {
+                    console.error(e)
+                }
+
+                agoraRTC.subscribe(user, mediaType).then(() => {
+                    if (mediaType === "video" && user.videoTrack) {
+                        const array = Array.from(users)
+                        array.push(user)
+                        setUsers(array)
+                    }
+                    if (mediaType === "audio") {
+                        user.audioTrack?.play()
+                    }
+                })
+            })
+            agoraRTC.on("user-unpublished", (user, mediaType) => {
+                console.log("unpublish", user, mediaType)
+                const array = Array.from(users)
+                users.splice(array.findIndex(it => it.uid === user.uid), 1)
+                setUsers(array)
+            })
         })
-        agoraRTC.on("user-unpublished", (user, mediaType) => {
-            console.log("unpublish", user, mediaType)
-            const array = Array.from(users)
-            users.splice(array.findIndex(it=>it.uid===user.uid),1)
-            setUsers(array)
-        })
-    })}
+    }
 
     return (
         <>
 
             <div style={{height: "100vh", width: "100vw", display: "flex", flexDirection: "column"}}>
-                {currentChannel?<div style={{height: 36}}>{`Current Channel:${currentChannel} Your Account:${account}`}</div>:<>
-                    <><label htmlFor={"channel"}>
-                    <span>Channel:</span>
-                    <input value={channel} onChange={event => setChannel(event.target.value)} id={"channel"}
-                           />
-                </label>
-                <label htmlFor={"account"}>
-                    <span>Account:</span>
-                    <input value={account} onChange={event => setAccount(event.target.value)} id={"account"}
-                           />
-                </label>
-                <button onClick={joinChannel}>Join</button>
-                    </>
-                </>}
+                {currentChannel ?
+                    <div style={{height: 36}}>{`Current Channel:${currentChannel} Your Account:${account}`}</div> : <>
+                        <><label htmlFor={"channel"}>
+                            <span>Channel:</span>
+                            <input value={channel} onChange={event => setChannel(event.target.value)} id={"channel"}
+                            />
+                        </label>
+                            <label htmlFor={"account"}>
+                                <span>Account:</span>
+                                <input value={account} onChange={event => setAccount(event.target.value)} id={"account"}
+                                />
+                            </label>
+                            <button onClick={joinChannel}>Join</button>
+                        </>
+                    </>}
 
                 <div style={{
                     height: "80%",
                     width: "100%",
                     display: "flex",
-                    flex:1,
+                    flex: 1,
                     flexDirection: "row",
                     justifyContent: "space-between"
                 }}>
@@ -122,29 +140,80 @@ function App() {
                         {users.map(item => {
 
                             return (
-                                <div style={{height: "25%", width: "100%", border: "1px solid green"}} key={item.uid}><Video
-                                    video={item.videoTrack!!}
+                                <div style={{height: "25%", width: "100%", border: "1px solid green"}} key={item.uid}>
+                                    <Video
+                                        video={item.videoTrack!!}
                                     />
                                 </div>)
                         })}
                     </div>
                 </div>
-                {rtmChannel?<div style={{display:"flex",height:"calc(20vh - 36px)"}}>
-                    <div style={{overflowY:"auto",flex:1}} ref={ref=>messageContainerRef.current=ref}>
-                        {logs.map((log,index)=>{
-                            return <p key={index}>{log}</p>
+                {rtmChannel ? <div style={{display: "flex", height: "calc(20vh - 36px)"}}>
+                    <div style={{overflowY: "auto", flex: 1}} ref={ref => messageContainerRef.current = ref}>
+                        {messages.map((log, index) => {
+                            if (log.image){
+                                return <div> {log.memberId}: <img src={log.image} key={index}/></div>
+                            }else {
+                                return <p key={index}>{log.memberId}: {log.text}</p>
+                            }
                         })}
                     </div>
-                    <div style={{alignSelf:"center"}}>
-                        <input  value={message} onChange={event => setMessage(event.target.value)} placeholder={""}/> <button onClick={async ()=> {
-                        if (message){
-                            await rtmChannel.sendMessage({text:message})
-                            pushTextMessage(account,message)
-                            setMessage("")
-                        }
-                    }}>Send</button>
+                    <div style={{alignSelf: "center"}}>
+                        <input value={message} onChange={event => setMessage(event.target.value)} placeholder={""}/>
+                        <button onClick={async () => {
+                            if (message) {
+                                await rtmChannel.sendMessage({text: message})
+                                pushMessage({memberId: account, text: message})
+                                setMessage("")
+                            }
+                        }}>Send
+                        </button>
+                        <label htmlFor={"sendFileInput"}>
+                                Send File
+                            <input type={"file"} id={"sendFileInput"} style={{width: 0, height: 0}}
+                                   onChange={(event) => {
+                                       let file = (event.target.files || [])[0];
+                                       let fileReader = new FileReader();
+                                       fileReader.onload = () => {
+                                           console.log("onload")
+                                           let result = fileReader.result as string;
+                                           let image = new Image();
+                                           image.onload = ev => {
+                                               agoraRTM.createMediaMessageByUploading(file, {
+                                                   messageType: "IMAGE",
+                                                   fileName: "agora.jpg",
+                                                   description: "test",
+                                                   thumbnail: file,
+                                                   width: image.width,
+                                                   height: image.height,
+                                                   thumbnailWidth: 50,
+                                                   thumbnailHeight: 50
+                                               }).then(mediaMessage => {
+                                                   let imageMessage = agoraRTM.createMessage({
+                                                       messageType: "IMAGE",
+                                                       mediaId: mediaMessage.mediaId,
+                                                       fileName:"example.jpg",
+                                                       description:"send image",
+                                                       thumbnail:file,
+                                                       width: image.width,
+                                                       height: image.height,
+                                                       thumbnailWidth: 50,
+                                                       thumbnailHeight: 50
+                                                   });
+                                                       rtmChannel.sendMessage(imageMessage).then(()=>{
+                                                           pushMessage({memberId:account,image:result})
+                                                       })
+                                               })
+                                           }
+                                           image.src = result
+
+                                       }
+                                       fileReader.readAsDataURL(file)
+
+                                   }}/>
+                        </label>
                     </div>
-                </div>:null}
+                </div> : null}
 
             </div>
         </>
